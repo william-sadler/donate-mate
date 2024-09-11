b7474076380104b897e39448111d941a7b004376
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-12 09:32:13 +1200

    nav placed in App for testing, all working

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,5 @@
 import { useFruits } from '../hooks/useFruits.ts'
+import Nav from './Nav.tsx'
 
 function App() {
   const { data } = useFruits()
@@ -6,9 +7,11 @@ function App() {
   return (
     <>
       <div className="app">
+        <Nav />
         <h1 className="text-3xl font-bold underline">
           Fullstack Boilerplate - with Fruits!
         </h1>
+
         <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
       </div>
     </>
