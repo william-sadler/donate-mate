fb679abca8f4c04ace84e8a6a08b86176e7bd923
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-12 10:01:47 +1200

    commit to test route

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,9 +1,11 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import App from './components/App.tsx'
 import Layout from './components/Layout.tsx'
+import Nav from './components/Nav.tsx'
 
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<App />} />
+    <Route path="/users" element={<Nav />} />
   </Route>,
 )
