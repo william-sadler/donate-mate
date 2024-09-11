ff024f5041431099610ff5e7f28fada93f7f6a25
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-11 16:24:18 +1200

    useUser and useAddUser hooks for auth0

--- client/hooks/useUsers.ts
+++ client/hooks/useUsers.ts
@@ -1,5 +1,42 @@
-import { MutationFunction, useQuery} from '@tanstack/react-query'
+import { MutationFunction, useQuery } from '@tanstack/react-query'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
-import * as API from '../api'
+import * as API from '../apis/apiUsers'
+import { useAuth0 } from '@auth0/auth0-react'
 
+export function useUsers() {
+  const { user, getAccessTokenSilently } = useAuth0()
+
+  const query = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const token = await getAccessTokenSilently()
+      return API.getUsers({ token })
+    },
+    enabled: !!user,
+  })
+
+  return {
+    ...query,
+    add: useAddUser(),
+  }
+}
+
+export function useUserMutation<TData = unknown, TVariables = unknown>(
+  mutationFn: MutationFunction<TData, TVariables>,
+) {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] })
+    },
+  })
+
+  return mutation
+}
+
+export function useAddUser() {
+  return useUserMutation(API.addUser)
+}
