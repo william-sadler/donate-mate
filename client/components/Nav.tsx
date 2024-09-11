7948a7f572253ef5d898bb75aa6547ef14585fe8
Fran.Berridge <frankieberry2024@outlook.com>
2024-09-11 19:15:48 +1200

    Started Nav component

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,13 +1,36 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import { IfAuthenticated, IfNotAuthenticated } from "./Authenticated";
+import { useAuth0 } from '@auth0/auth0-react'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
+import { useUsers } from '../hooks/useUsers.ts'
 
 function Nav() {
+  const { user, logout, loginWithRedirect } = useAuth0()
+  const { data: user } = useUsers()
 
-
-  const handleSignOut =() => {
+  const handleSignOut = () => {
     console.log('sign out')
     logout()
   }
 
-  const handleSignIn = () => 
+  const handleSignIn = () => {
+    console.log('sign in')
+    loginWithRedirect({
+      authorizationParams: {
+        redirectUrl: `${window.location.origin}/register`,
+      },
+    })
+  }
+
+  return (
+    <>
+      <IfAuthenticated>
+        <button onClick={handleSignOut}>Log out</button>
+        {user && <p> Signed in as: {user?.name}</p>}
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <button onClick={handleSignIn}>Login</button>
+      </IfNotAuthenticated>
+    </>
+  )
 }
+
+export default Nav
