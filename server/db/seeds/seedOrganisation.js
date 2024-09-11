98d0786f415e4a99f611759cf72f304275b2b998
Warkina Tujuba <warkina.tujuba@gmail.com>
2024-09-11 16:32:49 +1200

    basics of data structures

---
+++ server/db/seeds/seedOrganisation.js
@@ -0,0 +1,38 @@
+export async function seed(knex) {
+  // Deletes ALL existing entries
+  await knex('organisations').del()
+
+  // Inserts seed entries
+  await knex('organisations').insert([
+    {
+      name: 'Hospice Cuba Street',
+      contact_details: '123 Cuba St, Wellington',
+      about: 'Providing essential services to the community.',
+      longitude: 174.7762,
+      latitude: -41.2924,
+      org_types: 'Charity',
+      volunteering_needed: true,
+      method: 'Email',
+    },
+    {
+      name: 'Salvation Army Miramar',
+      contact_details: '50 Miramar Ave, Wellington',
+      about: 'Supporting those in need.',
+      longitude: 174.8287,
+      latitude: -41.3137,
+      org_types: 'Non-Profit',
+      volunteering_needed: false,
+      method: 'Phone',
+    },
+    {
+      name: 'Aro Valley Opshop',
+      contact_details: 'Aro St, Wellington',
+      about: 'Affordable goods and support for the local community.',
+      longitude: 174.7668,
+      latitude: -41.2973,
+      org_types: 'Charity',
+      volunteering_needed: true,
+      method: 'In-Person',
+    },
+  ])
+}
