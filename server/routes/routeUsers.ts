5d3f4b8e45525e04948131e39bf4533a42099776
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-11 15:21:10 +1200

    api routes for db, get and post user

--- server/routes/routeUsers.ts
+++ server/routes/routeUsers.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import checkJwt, { JwtRequest } from '../auth0.ts'
+import { StatusCodes } from 'http-status-codes'
 
 import * as db from '../db/dbUsers.ts'
 
 const router = Router()
 export default router
+
+// GET user
 
 router.get('/', checkJwt, async (req: JwtRequest, res) => {
   const auth0Id = req.auth?.sub
@@ -20,5 +23,32 @@ router.get('/', checkJwt, async (req: JwtRequest, res) => {
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'something went wrong' })
+  }
+})
+
+// POST newUser
+
+router.post('/', checkJwt, async (req: JwtRequest, res) => {
+  const auth0Id = req.auth?.sub
+  const { name, email } = req.body
+
+  if (!auth0Id) {
+    return res.sendStatus(StatusCodes.UNAUTHORIZED)
+  }
+
+  if (!name) {
+    return res.sendStatus(StatusCodes.NOT_FOUND)
+  }
+
+  if (!email) {
+    return res.sendStatus(StatusCodes.NOT_FOUND)
+  }
+
+  try {
+    await db.postUser(auth0Id, { name, email })
+    res.sendStatus(StatusCodes.CREATED)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('failed to add new user')
   }
 })
