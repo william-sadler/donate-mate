a9ef6820f653e971deafd9bd8691541628660bc5
william sadler <williamsadler37@gmail.com>
2024-09-13 10:27:59 +1200

    further implemention of front-end handling

--- client/components/EditOrgForm.tsx
+++ client/components/EditOrgForm.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 import EditCard from './EditCard.tsx'
 import { Organisation } from '../../models/modelOrganisations.ts'
 import { useOrganisationsById } from '../hooks/useOrganisations.ts'
+import EditHowToAdd from './EditHowToAdd.tsx'
+import EditCurrentlyAccepting from './EditCurrentlyAccepting.tsx'
+import EditAbout from './EditAbout.tsx'
 
 interface Props {
   organisation: Organisation
@@ -13,14 +15,27 @@ interface Props {
 type FormState = {
   orgName: string
   orgContactDetails: string
+  orgAbout: string
+  orgLongitude: number
+  orgLatitude: number
+  orgTypes: string
+  orgImage: string
+  orgVolunteeringNeeded: boolean
+  orgMethod: string
 }
 
 export default function EditOrgForm({ organisation, onUpdate }: Props) {
   const { getAccessTokenSilently } = useAuth0()
-  const navigate = useNavigate()
   const [form, setForm] = useState<FormState>({
     orgName: '',
     orgContactDetails: '',
+    orgAbout: '',
+    orgLongitude: 0,
+    orgLatitude: 0,
+    orgTypes: '',
+    orgImage: '',
+    orgVolunteeringNeeded: false,
+    orgMethod: '',
   })
 
   const org = useOrganisationsById(organisation.id)
@@ -41,21 +56,25 @@ export default function EditOrgForm({ organisation, onUpdate }: Props) {
     if (event) {
       event.preventDefault()
     }
-    org.patchOrgData.mutate({
-      id: organisation.id,
-      token: token,
-      orgData: {
-        name: form.name,
-        contactDetails: form.orgContactDetails,
-        about: form.about,
-        longitude: organisation.longitude,
-        latitude: organisation.latitude,
-        orgTypes: form.orgTypes,
-        image: form.image,
-        volunteeringNeeded: form.volunteeringNeeded,
-        method: form.method,
+    org.patchOrgData.mutate(
+      {
+        id: organisation.id,
+        token: token,
+        orgData: {
+          id: 0,
+          name: form.orgName,
+          contactDetails: form.orgContactDetails,
+          about: form.orgAbout,
+          longitude: organisation.longitude,
+          latitude: organisation.latitude,
+          orgTypes: form.orgTypes,
+          image: form.orgImage,
+          volunteeringNeeded: form.orgVolunteeringNeeded,
+          method: form.orgMethod,
+        },
       },
-    })
+      mutationOptions,
+    )
   }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -75,6 +94,17 @@ export default function EditOrgForm({ organisation, onUpdate }: Props) {
           orgContactDetails={form.orgContactDetails}
           handleChange={() => handleChange}
         />
+        <EditAbout
+          form={organisation}
+          orgAbout={form.orgAbout}
+          handleChange={() => handleChange}
+        />
+        <EditHowToAdd
+          form={organisation}
+          orgHowToAdd={form.orgMethod}
+          handleChange={() => handleChange}
+        />
+        <EditCurrentlyAccepting form={[]} handleUpdate={} />
       </form>
     </>
   )
