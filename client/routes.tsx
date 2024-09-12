5c35dbd29bf690be73c131ec61d3601e9ee795e0
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-12 12:14:00 +1200

    nav removed from client routes

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,11 +1,9 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import App from './components/App.tsx'
 import Layout from './components/Layout.tsx'
-import Nav from './components/Nav.tsx'
 
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<App />} />
-    <Route path="/users" element={<Nav />} />
   </Route>,
 )
