37a42598ba0f4f885c709fa3f18480fd68ae0457
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-13 10:11:03 +1200

    merged

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -1,10 +1,14 @@
 import Nav from './NavLogin'
-import iconLogo from '../../public/images/iconLogo.png'
+
 export default function NavBar() {
   return (
     <>
       <div className="bg-lightTeal flex items-center">
-        <img src={iconLogo} alt="logo for donateMate" className="logo" />
+        <img
+          src={'/images/iconLogo.png'}
+          alt="logo for donateMate"
+          className="logo"
+        />
 
         <h1 className="heading-1 caveat-bold -translate-y-1/8 ml-1">
           DonateMate
