ff9fa245cca0032353496d5c88c14776c15d4dec
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-12 19:11:11 +1200

    colors added to tailwind.config, title change

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,14 @@ export default {
     extend: {
       colors: {
         current: 'currentColor',
+        light: '#fbe8d1',
         brightOrange: '#faa51d',
         lightGreen: '#c6eb7a',
-        mainTeal: '#30b79e',
-        darkerTeal: '#1ef56D',
-        textBlue: '#162f50',
+        lime: '#79b304',
+        lightTeal: '#30b79e',
+        darkerTeal: '#1e756D',
+        blue: '#162f50',
+        textBlue: '#0b1828',
       },
     },
   },
