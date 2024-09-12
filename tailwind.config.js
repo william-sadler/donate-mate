cd7dcc328968ac3388e5e1aba010eed43841f9f6
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-12 15:52:47 +1200

    some initial colors and a logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './client/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        current: 'currentColor',
+        brightOrange: '#faa51d',
+        lightGreen: '#c6eb7a',
+        mainTeal: '#30b79e',
+        darkerTeal: '#1ef56D',
+        textBlue: '#162f50',
+      },
+    },
   },
   plugins: [],
 }
