8fc7ecd8f887050c97f9ee475a306828623b2103
eric-mokuma <mokumaeric@gmail.com>
2024-09-12 16:21:43 +1200

    Card urgently status complete

---
+++ client/components/CardUrgentlyStatus.tsx
@@ -0,0 +1,22 @@
+import { useTypes } from '../hooks/useTypes'
+
+interface Props {
+  id: number
+}
+
+export default function CarUrgentlyStatus({ id }: Props) {
+  const { data, isPending, isError, error } = useTypes(id)
+
+  if (isPending) {
+    return <p>Loading...</p>
+  }
+  if (isError) {
+    return <p>Error: {error.message}</p>
+  }
+
+  return (
+    data.find((type) => type.urgentlySeeking === true) && (
+      <div>Urgently Seeking</div>
+    )
+  )
+}
