21a9df3fbf98fb289debd26561cab64b1cdb2fd3
Fran.Berridge <frankieberry2024@outlook.com>
2024-09-13 14:10:25 +1200

    Created types and currently accepting on profile

--- client/pages/OrgProfilePage.tsx
+++ client/pages/OrgProfilePage.tsx
@@ -1,5 +1,7 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import { useOrganisationsById } from '../hooks/useOrganisations'
+import CurrentlyAccepting from '../components/ProfileCurrentlyAccepting'
+import { useTypes } from '../hooks/useTypes'
 
 export default function OrgProfilePage() {
   const param = useParams()
@@ -7,6 +9,7 @@ export default function OrgProfilePage() {
   const id = Number(param.id)
   const { data, isPending, isError, error, failureCount } =
     useOrganisationsById(id)
+  const typeData = useTypes(id)
 
   if (isPending || !data) {
     let failures = ''
@@ -23,6 +26,21 @@ export default function OrgProfilePage() {
     return <p>Failed to get Org: {error.message}</p>
   }
 
+  if (typeData.isPending || !typeData.data) {
+    let failures = ''
+    if (typeData.failureCount > 0) {
+      failures = ` (failed ${typeData.failureCount} times)`
+    }
+    if (typeData.failureCount > 3) {
+      navigate('/')
+    }
+    return <div>Loading... {failures}</div>
+  }
+
+  if (typeData.isError) {
+    return <p>Failed to get Org: {typeData.error.message}</p>
+  }
+
   return (
     <>
       <h2>{data.name}</h2>
@@ -30,6 +48,9 @@ export default function OrgProfilePage() {
       <p>{data.contactDetails}</p>
       <p>{data.method}</p>
       <p>{data.about}</p>
+      <CurrentlyAccepting
+        typeData={typeData.data}
+      />
     </>
   )
 }
