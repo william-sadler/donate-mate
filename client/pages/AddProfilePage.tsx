ba46fb4c03b14dbc0a72d1494d2169f753e1d82e
william sadler <williamsadler37@gmail.com>
2024-09-14 01:17:53 +1200

    resolved tailwind bug

--- client/pages/AddProfilePage.tsx
+++ client/pages/AddProfilePage.tsx
@@ -173,7 +173,7 @@ export default function AddProfilePage() {
                   <button
                     onClick={() => navigate('/')}
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    className="custom-button"
                   >
                     Continue
                   </button>
