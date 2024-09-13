f2313e89abbebabae337f39c0a225704de2c0730
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-13 16:43:56 +1200

    org db filled, edits to data access on OrgProfile page, org model updated

--- models/modelOrganisations.ts
+++ models/modelOrganisations.ts
@@ -1,15 +1,15 @@
 export interface Organisation {
   id: number
   name: string
-  contact_email: string | null
-  contact_number: string | null
+  contactEmail: string | null
+  contactNumber: string | null
   location: string
   about: string
   longitude: number | null
   latitude: number | null
-  org_types: string
+  orgTypes: string
   image: string
-  volunteering_needed: boolean
-  donation_method: string | null
+  volunteeringNeeded: boolean
+  donationMethod: string | null
   website?: string
 }
