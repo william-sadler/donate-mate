04cee899d09015d1655832c5da8495e64b1742fb
eric-mokuma <mokumaeric@gmail.com>
2024-09-13 12:03:05 +1200

    Urgently hover complete

--- client/components/LandingCard.tsx
+++ client/components/LandingCard.tsx
@@ -9,13 +9,19 @@ interface Props {
 }
 
 export default function LandingCard({ name, src, alt, orgId }: Props) {
+  const isUrgent = CardUrgentlyStatus({ id: orgId })
+
   return (
-    <div className="image-card">
-      <div>
-        <CardUrgentlyStatus id={orgId} />
-      </div>
+    <div className="landing-card">
       <img className="image" src={src} alt={alt} />
-      <div className="name">{name}</div>
+      <div className="name">
+        {name}
+        {isUrgent && (
+          <div className="red-cycle">
+            <div className="circle"></div>
+          </div>
+        )}
+      </div>
       <CardTypes id={orgId} />
     </div>
   )
