7f1e792bc7499b99ce3d98b2592bd160267abc20
william sadler <williamsadler37@gmail.com>
2024-09-14 12:16:30 +1200

    resolved styling issues electric boogaloo

--- client/components/AddOrgForm.tsx
+++ client/components/AddOrgForm.tsx
@@ -168,9 +168,9 @@ export default function AddOrgForm({
               <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-200 sm:mx-0 sm:h-10 sm:w-10">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
                       <svg
-                        className="h-6 w-6 text-green-500"
+                        className="h-6 w-6 text-green-600"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth="1.5"
--- client/pages/AddProfilePage.tsx
+++ client/pages/AddProfilePage.tsx
@@ -137,9 +137,9 @@ export default function AddProfilePage() {
               <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <div className="custom-svg mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
                       <svg
-                        className="h-6 w-6 text-red-600"
+                        className="h-6 w-6 text-red-500"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth="1.5"
