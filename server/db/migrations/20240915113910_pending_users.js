e12c9001d7874cc4c34b12f846a4dc4960c7b55c
william sadler <williamsadler37@gmail.com>
2024-09-15 23:53:49 +1200

    intial backend implementation complete

--- server/db/migrations/20240915113910_pending_users.js
+++ server/db/migrations/20240915113910_pending_users.js
@@ -1,13 +1,12 @@
 export async function up(knex) {
-  await knex.schema.createTable('users', (table) => {
+  await knex.schema.createTable('pending_users', (table) => {
     table.string('auth0Id').primary()
     table.string('name')
     table.string('email')
     table.integer('org_id')
-    table.boolean('is_owner')
   })
 }
 
 export async function down(knex) {
-  await knex.schema.dropTable('users')
+  await knex.schema.dropTable('pending_users')
 }
