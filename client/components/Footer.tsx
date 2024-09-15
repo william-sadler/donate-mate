174051c42657af688d480e6ffdef4cd11ff5573f
Fran.Berridge <frankieberry2024@outlook.com>
2024-09-16 11:49:43 +1200

    Adding footer

---
+++ client/components/Footer.tsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <div className="flex min-h-[20svh] items-center justify-between bg-gradient-to-b from-brightTeal to-lightTeal px-4 py-2">
+      <div className="footer-menu">
+        <div className="row copyright">
+
+
+          {/* <div className="footer-menu">
+            <p className="">Home</p>
+            <p className="">F.A.Q</p>
+            <p className="">Cookies Policy</p>
+            <p className="">Terms Of Service</p>
+            <p className="">Support</p>
+          </div>
+          <p>Copyright &copy; 2024</p> */}
+
+        
+
+        </div>
+      </div>
+    </div>
+  )
+}
