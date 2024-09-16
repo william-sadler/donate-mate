b37770f64ade8345fe71d1aaff4527a3b3cafe96
Fran.Berridge <frankieberry2024@outlook.com>
2024-09-16 15:20:03 +1200

    updated footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,23 +1,31 @@
 export default function Footer() {
   return (
-    <div className="flex min-h-[20svh] items-center justify-between bg-gradient-to-b from-brightTeal to-lightTeal px-4 py-2">
-      <div className="footer-menu">
-        <div className="row copyright">
-
-
-          {/* <div className="footer-menu">
-            <p className="">Home</p>
-            <p className="">F.A.Q</p>
-            <p className="">Cookies Policy</p>
-            <p className="">Terms Of Service</p>
-            <p className="">Support</p>
+    <>
+      <footer className="px20 min-h-[10svh] bg-gradient-to-b from-brightTeal to-lightTeal py-5 pl-10">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          <div>
+            <img
+              src="/images/iconLogo.png"
+              className="mr-5 h-6 sm:h-9"
+              alt="logo"
+            />
           </div>
-          <p>Copyright &copy; 2024</p> */}
-
-        
-
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-4">
+            <div>
+              <p className="font-medium">About</p>
+            </div>
+            <div>
+              <p className="font-medium">Social Media</p>
+            </div>
+            <div>
+              <p className="font-medium">Useful Links</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+        <p className="mt-8 text-xs text-gray-800">
+          © Mako-Kōtare 2024 | DonateMate Limited
+        </p>
+      </footer>
+    </>
   )
 }
