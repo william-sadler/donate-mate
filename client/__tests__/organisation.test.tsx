3368d849f0e1dcfc0bc0981d1646149d43c90871
william sadler <williamsadler37@gmail.com>
2024-09-17 15:47:38 +1200

    styling for navlogin

--- client/__tests__/organisation.test.tsx
+++ client/__tests__/organisation.test.tsx
@@ -4,6 +4,341 @@ import { renderRoute } from './setup.tsx'
 
 import nock from 'nock'
 
+const date = new Date().getTime()
+
+const ALL_TYPES = [
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: true,
+    organisation_id: 1,
+    date: `${date}`,
+  },
+  {
+    name: 'Furniture',
+    accepting: true,
+    urgently_seeking: true,
+    organisation_id: 1,
+    date: `${date}`,
+  },
+
+  // id: 2
+  // name: 'Salvation Army Miramar'
+
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 2,
+    date: `${date}`,
+  },
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 2,
+    date: `${date}`,
+  },
+
+  // id: 3
+  // name: 'Whiskers & Wonders'
+
+  {
+    name: 'Pet care',
+    accepting: true,
+    urgently_seeking: true,
+    organisation_id: 3,
+    date: `${date}`,
+  },
+  {
+    name: 'Household goods',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 3,
+    date: `${date}`,
+  },
+  {
+    name: 'Clothes',
+    accepting: false,
+    urgently_seeking: false,
+    organisation_id: 3,
+    date: `${date}`,
+  },
+
+  // id: 4
+  // name: 'Aro Valley Opshop'
+
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 4,
+    date: `${date}`,
+  },
+  {
+    name: 'Household goods',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 4,
+    date: `${date}`,
+  },
+
+  // id: 5
+  // name: 'The Koha Shed Wellington'
+
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 5,
+    date: `${date}`,
+  },
+  {
+    name: 'Furniture',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 5,
+    date: `${date}`,
+  },
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 5,
+    date: `${date}`,
+  },
+
+  // id: 6
+  // name: 'Dress for Success Wellington'
+
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 6,
+    date: `${date}`,
+  },
+  {
+    name: 'Accessories',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 6,
+    date: `${date}`,
+  },
+
+  // id: 7
+  // name: 'Wellington SPCA'
+
+  {
+    name: 'Pet care',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 7,
+    date: `${date}`,
+  },
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 7,
+    date: `${date}`,
+  },
+  {
+    name: 'Appliances',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 7,
+    date: `${date}`,
+  },
+
+  // id: 8
+  // name: 'Salvation Army Wellington'
+
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 8,
+    date: `${date}`,
+  },
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 8,
+    date: `${date}`,
+  },
+  {
+    name: 'Furniture',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 8,
+    date: `${date}`,
+  },
+
+  // id: 9
+  // name: 'Wellington City Mission'
+
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 9,
+    date: `${date}`,
+  },
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 9,
+    date: `${date}`,
+  },
+
+  // id: 10
+  // name: 'Vinnies Wellington'
+
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 10,
+    date: `${date}`,
+  },
+  {
+    name: 'Furniture',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 10,
+    date: `${date}`,
+  },
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 10,
+    date: `${date}`,
+  },
+  {
+    name: 'Household goods',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 10,
+    date: `${date}`,
+  },
+
+  // id: 11
+  // name: "Wellington Women's Refuge"
+
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 11,
+    date: `${date}`,
+  },
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 11,
+    date: `${date}`,
+  },
+  {
+    name: 'Toiletries',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 11,
+    date: `${date}`,
+  },
+
+  // id: 12
+  // name: 'Newtown Community & Cultural Centre'
+
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 12,
+    date: `${date}`,
+  },
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 12,
+    date: `${date}`,
+  },
+
+  // id: 13
+  // name: 'Kaibosh Wellington'
+
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 13,
+    date: `${date}`,
+  },
+
+  // id: 14
+  // name: 'The Free Store'
+
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 14,
+    date: `${date}`,
+  },
+
+  // id: 15
+  // name: 'Wellington Night Shelter'
+
+  {
+    name: 'Clothes',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 15,
+    date: `${date}`,
+  },
+  {
+    name: 'Linen & Bedding',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 15,
+    date: `${date}`,
+  },
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 15,
+    date: `${date}`,
+  },
+
+  // id: 16
+  // name: 'Soup Kitchen Wellington'
+
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 16,
+    date: `${date}`,
+  },
+
+  // id: 17
+  // name: 'Soup Kitchen Wellington'
+
+  {
+    name: 'Food',
+    accepting: true,
+    urgently_seeking: false,
+    organisation_id: 17,
+    date: `${date}`,
+  },
+]
+
 const INITIAL_ORGS = [
   {
     id: 1,
@@ -296,6 +631,8 @@ describe('a user can filter organisations by donation types', () => {
     nock('http://localhost')
       .get('/api/v1/organisations')
       .reply(200, INITIAL_ORGS)
+
+    nock('http://localhost').get('/api/v1/alltypes').reply(200, ALL_TYPES)
 
     // Render the route or component
     const { findByRole } = renderRoute()
